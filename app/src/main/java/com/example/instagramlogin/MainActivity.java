a8b36bc3e4766372dc5571580308a48169e8a747
manhdhdn <huumanh.bmt.dn@gmail.com>
2023-01-30 13:02:39 +0700

    Add project files.

---
+++ app/src/main/java/com/example/instagramlogin/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.instagramlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.textInputLayout1);
+        password = findViewById(R.id.textInputLayout2);
+        loginButton = findViewById(R.id.button);
+    loginButton.setOnClickListener(view -> {
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("123456")){
+            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+
+        }
+    });
+    }
+}
